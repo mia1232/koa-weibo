0bb0ac7ce368a9b3f8ac11bbfb26f32eea486052
mia1232 <774640444@qq.com>
2020-03-23 15:47:12 +1100

    路由的演示

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 const router = require('koa-router')()
 
+
+router.prefix('/users');
+
 router.get('/', async (ctx, next) => {
   await ctx.render('index', {
     title: 'Hello Koa 2!'
@@ -13,6 +16,33 @@ router.get('/string', async (ctx, next) => {
 router.get('/json', async (ctx, next) => {
   ctx.body = {
     title: 'koa2 json'
+  }
+})
+
+
+router.get('/profile/:userName', async (ctx, next) => {
+  const { userName } = ctx.params;
+  ctx.body = {
+    title: 'this is profile page',
+    userName
+  }
+})
+
+router.get('/loadMore/:userName/:pageIndex', async (ctx, next) => {
+  const { pageIndex, userName } = ctx.params;
+  ctx.body = {
+    title: 'this is profile page',
+    userName,
+    pageIndex
+  }
+})
+
+router.post('/login', async (ctx, next) => {
+  const { userName, password } = ctx.request.body;
+  ctx.body = {
+    tag: 101,
+    userName,
+    password
   }
 })
 
